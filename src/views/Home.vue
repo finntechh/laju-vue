ca405cc07493f779737eaa114e632881ddc06669
Finn <fhertsch235@gmail.com>
2024-03-17 22:26:12 +0100

    home beginnig

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="Home">
-        Home view
+    <div class="home-content">
+        <h2 class="home-heading">
+          Willkommen
+        </h2>
+        <span class="home-description">
+          <p>
+            Hier können Sie sich über unsere Events und aktuelle
+            Entwicklungen informieren.
+          </p>
+        </span>
     </div>
 </template>
+
+<script setup>
+import "../assets/css/home.css";
+</script>
